8630038d4ebceca5183026ee9526e1214c27ecd4
Lucca Nascimento <luccadev@hotmail.com>
2026-03-19 10:51:41 -0300

    first commit

---
+++ user/src/test/java/dev/lucca/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lucca.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
